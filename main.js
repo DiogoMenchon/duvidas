5477df7292a4d039be620d42d17ed7b955724a54
Diogo Peres <plaer654@gmail.com>
2023-08-25 09:17:53 -0300

    erros na digitação

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const form = document.getElementById('form-atividade')
-form.addEventListener('submit', function(e){
-    e. preventdefault();
+let linhas = '';
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade')
 
     let linha = '<tr>'
-    linha +=`<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNomeAtividade.value}</td>`;
     linha += `<td>${inputNotaAtividade.value}</td>`;
     linha += `<td>${inputNotaAtividade.value >= 7 ? 'Aprovado' : 'Reprovado'}</td>`;
-    linha +=`</tr>`;
+    linha += `</tr>`;
 
-    linhas+= linha;
+    linhas += linha;
 
     const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linha;
+    corpoTabela.innerHTML += linha;
 });
