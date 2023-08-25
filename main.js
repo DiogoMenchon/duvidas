50a339acc55880597a90d7008c06d25ace95ee20
Diogo Peres <plaer654@gmail.com>
2023-08-25 09:50:59 -0300

    arrumando

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const form = document.getElementById('form-atividade')
+const imgAprovado = '<img src"./imagens/aprovado.png" alt=Emoji festejando/>';
+const imgReprovado = '<img src"./imagens/reprovado.png" alt=Emoji triste/>';
+
+
 let linhas = '';
+
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
@@ -9,11 +14,14 @@ form.addEventListener('submit', function (e) {
     let linha = '<tr>'
     linha += `<td>${inputNomeAtividade.value}</td>`;
     linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? 'Aprovado' : 'Reprovado'}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
     linha += `</tr>`;
 
     linhas += linha;
 
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML += linha;
+
+    inputNomeAtividade.value ='';
+    inputNotaAtividade.value ='';
 });
